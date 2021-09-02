c1fcc47baa45ee64ab4c9aad2ad6614af1924a62
lMatu <pereiramatin@gmail.com>
2021-09-02 09:06:59 -0300

    Ultimos cambios 2.0

--- resources/views/layouts/applogin.blade.php
+++ resources/views/layouts/applogin.blade.php
@@ -32,7 +32,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                    <!-- Esto hago en la rama de desarrollo -->
                     </ul>
 
                     <!-- Right Side Of Navbar -->
